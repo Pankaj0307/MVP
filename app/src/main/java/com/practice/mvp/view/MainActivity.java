289f05642fd6b23e291caf20efd99196e47a62ca
SF0038007 <pankaj.sharma@spandanaindia.com>
2019-04-24 17:30:02 +0530

    Changes in theme

--- app/src/main/java/com/practice/mvp/view/MainActivity.java
+++ app/src/main/java/com/practice/mvp/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.practice.mvp.view;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -28,8 +27,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityPrese
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         presenter = new MainActivityPresenter(this);
 
